230aaa777c68d6f0e1c51ff12454047e2547e6b3
Travis Lyons <travis.lyons@sourcegraph.com>
2026-04-28 10:59:23 -0400

    Initial commit

---
+++ src/main/java/dev/trly/java/example/primitive/wrappers/LaunchChecklistApp.java
@@ -0,0 +1,33 @@
+package dev.trly.java.example.primitive.wrappers;
+
+public final class LaunchChecklistApp {
+    private LaunchChecklistApp() {
+    }
+
+    public static void main(String[] args) {
+        final String[] steps = {
+            "power",
+            "network",
+            "storage",
+            "alerts"
+        };
+        final Boolean[] readiness = {
+            new Boolean(true),
+            new Boolean(true),
+            new Boolean(false),
+            new Boolean(true)
+        };
+
+        int readyCount = 0;
+        for (int index = 0; index < readiness.length; index++) {
+            final boolean ready = readiness[index].booleanValue();
+            final String status = ready ? "ready" : "waiting";
+            System.out.printf("%s: %s%n", steps[index], status);
+            if (ready) {
+                readyCount++;
+            }
+        }
+
+        System.out.printf("Summary: %d of %d steps are ready%n", readyCount, readiness.length);
+    }
+}
